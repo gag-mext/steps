017fb62ef13137a0a6ef40e2e0fde68676e310a8
姚嘉松 <yaojiasong@gooagoo.com>
2017-11-01 17:38:38 +0800

    组件转移 无线 UI 组件 steps

---
+++ demo/basic.tsx
@@ -0,0 +1,77 @@
+/* tslint:disable:jsx-no-multiline-js */
+import { Steps, WingBlank } from 'antd-mobile';
+const Step = Steps.Step;
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+
+export default class BasicTimelineExample extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      steps1: [
+        {title: 'Finished', description: 'This is description'},
+        {title: 'In Progress', description: 'This is description'},
+        {title: 'Waiting', description: 'This is description'},
+      ],
+      steps2: [
+        {title: 'Finished', description: 'This is description', status: 'finish'},
+        {title: 'In Progress', description: 'This is description', status: 'process'},
+        {title: 'Waiting', description: 'This is description', status: 'error'},
+        {title: 'Waiting', description: 'This is description', status: 'wait'},
+      ],
+    };
+  }
+  render() {
+    return (<ScrollView
+      style={{ flex: 1 }}
+      automaticallyAdjustContentInsets={false}
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
+    >
+      <View style={{marginTop: 60}}>
+        <WingBlank size="lg">
+          <Steps size="small" current={1}>
+            {
+              this.state.steps1.map((item, index) => (
+                <Step key={index} title={item.title} description={item.description} status={item.status} />
+              ))
+            }
+          </Steps>
+        </WingBlank>
+      </View>
+      <View>
+        <WingBlank size="lg">
+          <Steps size="small">
+            {
+              this.state.steps2.map((item, index) => (
+                <Step key={index} title={item.title} description={item.description} status={item.status} />
+              ))
+            }
+          </Steps>
+        </WingBlank>
+      </View>
+      <View>
+        <WingBlank size="lg">
+          <Steps current={1}>
+            {
+              this.state.steps1.map((item, index) => (
+                <Step key={index} title={item.title} description={item.description} status={item.status} />
+              ))
+            }
+          </Steps>
+        </WingBlank>
+      </View>
+      <View>
+        <WingBlank size="lg">
+          <Steps>
+            {
+              this.state.steps2.map((item, index) => (
+                <Step key={index} title={item.title} description={item.description} status={item.status} />
+              ))
+            }
+          </Steps>
+        </WingBlank>
+      </View>
+    </ScrollView>);
+  }
+}
---
+++ src/Steps.jsx
@@ -0,0 +1,75 @@
+import '../style';
+import React from 'react';
+import RcSteps from 'rc-steps';
+import Icon from '@gag/icon';
+
+
+class Steps extends React.Component {
+  static Step = RcSteps.Step;
+  stepRefs: any;
+
+  componentDidMount() {
+    this.componentDidUpdate();
+  }
+  componentDidUpdate() {
+    if (this.props.direction !== 'horizontal') {
+      return;
+    }
+    // set tail's left position based on main's width for each step dynamically.
+    this.stepRefs.forEach(s => {
+      if (s.refs.tail) {
+        s.refs.tail.style.left = `${s.refs.main.offsetWidth / 2}px`;
+      }
+    });
+  }
+  render() {
+    this.stepRefs = [];
+    const { children, status } = this.props;
+    const current = this.props.current;
+    // flattern the array at first https://github.com/ant-design/ant-design-mobile/issues/934
+    let newChildren: Array<any> = React.Children.map(children, item => item);
+    newChildren = React.Children.map(newChildren, (item: any, index) => {
+      let className = item.props.className;
+      if (index < newChildren.length - 1 && newChildren[index + 1].props.status === 'error') {
+        className = className ? `${className} error-tail` : 'error-tail';
+      }
+
+      let icon = item.props.icon;
+      if (!icon) {
+        if (index < current) {
+          // 对应 state: finish
+          icon = 'check-circle-o';
+        } else if (index > current) {
+          // 对应 state: wait
+          icon = 'ellipsis';
+          className = className ? `${className} ellipsis-item` : 'ellipsis-item';
+        }
+        if (status === 'error' && index === current || item.props.status === 'error') {
+          icon = 'cross-circle-o';
+        }
+      }
+      icon = typeof icon === 'string' ? <Icon type={icon} /> : icon;
+      return React.cloneElement(item, { icon, className, ref: c => this.stepRefs[index] = c });
+    });
+    return <RcSteps ref="rcSteps" {...this.props}>{newChildren}</RcSteps>;
+  }
+}
+Steps.defaultProps = {
+    prefixCls: 'am-steps',
+    iconPrefix: 'ant',
+    labelPlacement: 'vertical',
+    direction: 'vertical',
+    current: 0,
+};
+Steps.propTypes = {
+  prefixCls: React.PropTypes.string,
+  iconPrefix: React.PropTypes.string,
+  direction: React.PropTypes.string,
+  labelPlacement: React.PropTypes.string,
+  children: React.PropTypes.any,
+  status: React.PropTypes.string,
+  size: React.PropTypes.string,
+  current: React.PropTypes.number
+};
+Steps.displayName = "Steps";
+module.exports=Steps;
